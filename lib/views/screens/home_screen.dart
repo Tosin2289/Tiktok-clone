af1f899391eda5eb4472de5ec00c8df9631a8dc5
Tosin2289 <ayoolatosin00@gmail.com>
2023-11-15 23:51:46 +0100

    bottom navigation bar

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/constants.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+import '../widgets/custom_icon.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageindex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: backgroundColor,
+        onTap: (value) {
+          setState(() {
+            pageindex = value;
+          });
+        },
+        currentIndex: pageindex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 30), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search, size: 30), label: "Search"),
+          BottomNavigationBarItem(icon: CustomIcon(), label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.inbox, size: 30), label: "Inbox"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline, size: 30), label: "Profile"),
+        ],
+      ),
+      body: const Center(child: Text('HomeScreen')),
+    );
   }
 }
